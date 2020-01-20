f9f7e0ab31e443e61192be0ab3883be00b67c17e
sigit wasis subekti <sigitghoticmetal2001@gmail.com>
2020-01-20 11:35:44 +0700

    fundamental golang

---
+++ tipe-data.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Tipe data golang uint8 = 0 ↔ 255 
+	var number_a uint8 = 100
+
+	// Tipe data golang uint16 = 0 ↔ 65535
+	var number_b uint16 = 65535
+
+	// Tipe data golang uint32 = 0 ↔ 4294967295
+	var number_c uint32 = 4294967295
+
+	// Tipe data golang bool = true & false
+	var boolean bool = true
+
+	// Tipe data golang desimal
+	var number_desimal = 2.66
+
+	// Tipe data golang string
+	var pesan = "Hallo Sahabat Coding"
+
+	fmt.Println(number_a)
+	fmt.Println(number_b)
+	fmt.Println(number_c)
+	fmt.Println(boolean)
+	fmt.Println(number_desimal)
+	fmt.Println(pesan)
+}
---
+++ variable.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Variabel dengan menuliskan tipe data
+	var firstname string = "Sigit"
+	// Variabel tanpa menuliskan tipe data
+	var lastname = "Wasis Subekti"
+	// Variabel tanpa menuliskan var
+	address := "Lampung"
+	// Mengubah varibel address
+	address = "Lambar"
+	// Digunakan untuk menampilan test
+    fmt.Println("Nama Saya :", firstname, lastname, "Alamat :", address)
+}
