33fbbe75ce33d311d91dafbc53df725ab1dc6c85
sigit wasis subekti <sigitghoticmetal2001@gmail.com>
2020-01-25 21:47:30 +0700

    array golang

--- array.go
+++ array.go
@@ -3,7 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	
 	// Array (Kumpulan data bertipe data sama yang ada dalam suatu variabel)
-
+	// array dengan nama variabel bauh dengan tipe data string dan jumlah data di dalam array 4
+	var buah=[4]string{"apel", "jeruk", "anggur", "melon"}
+	// untuk merubah index 1 (jeruk) menjadi (mangga)
+	buah[1] = "mangga"
+	// Mencetak isi dan elemet buah
+	fmt.Println("Jumlah Element :", len(buah))
+	fmt.Println("Isi Element :", buah)
 }
