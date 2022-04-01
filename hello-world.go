25a9cf0e80a1db77549f2d30dd90f7f5a40b950b
sigit wasis subekti <sigitghoticmetal2001@gmail.com>
2022-04-01 14:05:28 +0700

    update fundamental variabel golang

--- hello-world.go
+++ hello-world.go
@@ -1,5 +1,10 @@
 package main
 
+/**
+Package fmt merupakan salah satu package bawaan yang disediakan oleh Go, isinya banyak fungsi untuk
+keperluan I/O yang berhubungan dengan text. Fungsi fmt.Println() dapat menampung parameter yang tidak terbatas jumlahnya.
+*/
+
 import "fmt"
 
 func main() {
--- variable.go
+++ variable.go
@@ -13,4 +13,54 @@ func main() {
 	address = "Lambar"
 	// Digunakan untuk menampilan test
     fmt.Println("Nama Saya :", firstname, lastname, "Alamat :", address)
+
+	/**
+	 - var di atas digunakan untuk deklarasi variabel.
+	 - skemanya adalah var <nama variabel> <tipe-data>
+	 - fmt.Printf digunakan untuk untuk menampilkan output dalam bentuk tertentu
+	 - fmt.Println digunakan untuk menampilkan output yang secara otomatis menghasilkan new line (baris baru) di akhir.
+	 - tanda + untuk penggabungan string
+	 - \n, untuk memunculkan baris baru di akhir
+	*/
+
+	// ---- DEKLARASI DENGAN TIPE DATA ----
+	var firstName string = "Sigit"
+
+	var lastName string
+	lastName = "Wasis Subekti"
+
+	fmt.Printf("halo john wick!\n")
+	fmt.Printf("hello %s %s \n", firstName, lastName);
+	fmt.Println("halo", firstName, lastName + "!")
+
+	/**
+	- lastNameBrother dideklarasikan dengan menggunakan metode type inference.
+	- Tipe data lastNameBrother secara otomatis akan ditentukan menyesuaikan value atau nilai-nya.
+	- Tanda := hanya digunakan sekali di awal pada saat deklarasi.
+	- Perlu diketahui, deklarasi menggunakan := hanya bisa dilakukan di dalam blok fungsi.
+	*/
+
+
+	// --- DEKLARASI TANPA TIPE DATA ---
+	var firstNameBrother string = "dwi"
+	lastNameBrother := "nur hamid"
+
+	fmt.Printf("Hai %s %s!\n", firstNameBrother, lastNameBrother);
+
+
+	// --- DEKLARASI MULTIPLE VARIABEL ---
+	// cara pertama
+	var first, second, third string
+	first, second, third = "satu", "dua", "tiga"
+
+	// cara kedua
+	var fourth, fiveth, sixth = "empat", "lima", "enam"
+
+	// cara ketiga
+	seventh, eight, ninth := "tujuh", "delapan", "sembilan"
+
+	fmt.Println(first, second, third)
+	fmt.Println(fourth, fiveth, sixth)
+	fmt.Println(seventh, eight, ninth)
+
 }
